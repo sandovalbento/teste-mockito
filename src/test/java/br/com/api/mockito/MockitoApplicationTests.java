5cda8ee5ed4bcd06b58e850938ca41581711ec64
sandovalbento <sandovalbento@gmail.com>
2022-10-10 18:36:09 -0300

    commti junit

--- src/test/java/br/com/api/mockito/MockitoApplicationTests.java
+++ src/test/java/br/com/api/mockito/MockitoApplicationTests.java
@@ -1,13 +1,15 @@
 package br.com.api.mockito;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MockitoApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void main() {
+		MockitoApplication.main(new String[]{});
 	}
 
 }
