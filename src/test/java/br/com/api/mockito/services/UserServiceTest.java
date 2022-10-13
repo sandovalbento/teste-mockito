caf05e29d70236c4e166692b760519d36ee26c93
sandovalbento <sandovalbento@gmail.com>
2022-10-12 22:13:29 -0300

    test delete

--- src/test/java/br/com/api/mockito/services/UserServiceTest.java
+++ src/test/java/br/com/api/mockito/services/UserServiceTest.java
@@ -2,6 +2,7 @@ package br.com.api.mockito.services;
 
 import br.com.api.mockito.dto.UserDTO;
 import br.com.api.mockito.entity.User;
+import br.com.api.mockito.exceptions.DataIntegrationViolationException;
 import br.com.api.mockito.exceptions.NotfoundException;
 import br.com.api.mockito.repository.UserRepository;
 import org.junit.jupiter.api.Assertions;
@@ -20,7 +21,7 @@ import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 class UserServiceTest {
 
@@ -97,17 +98,65 @@ class UserServiceTest {
     }
 
     @Test
+    void createViolation() {
+        when(userRepository.findByEmail(anyString())).thenReturn(optionalUser);
+        try {
+            optionalUser.get().setId(2);
+            userService.create(userDTO);
+        }catch (Exception e){
+            assertEquals(DataIntegrationViolationException.class, e.getClass());
+            assertEquals("E-mail já cadastrado no sistema", e.getMessage());
+        }
+    }
+
+    @Test
     void update() {
+        when(userRepository.save(any())).thenReturn(user);
+        when(userRepository.findByEmail("sandovalbento@gmail.com")).thenReturn(optionalUser);
+        User response = userService.update(userDTO);
+
+        assertNotNull(response);
+        assertEquals(User.class, response.getClass());
+        assertEquals("Sandoval", response.getName());
+        assertEquals("sandovalbento@gmail.com", response.getEmail());
+        assertEquals("1234", response.getPassword());
+    }
+
+    @Test
+    void updateViolation() {
+        when(userRepository.findByEmail(anyString())).thenReturn(optionalUser);
+        try {
+            optionalUser.get().setId(2);
+            userService.create(userDTO);
+        }catch (Exception e){
+            assertEquals(DataIntegrationViolationException.class, e.getClass());
+            assertEquals("E-mail já cadastrado no sistema", e.getMessage());
+        }
     }
 
     @Test
     void delete() {
+        when(userRepository.findById(anyInt())).thenReturn(optionalUser);
+        doNothing().when(userRepository).deleteById(anyInt());
+        userService.delete(1);
+        verify(userRepository, times(1)).deleteById(anyInt());
     }
+    @Test
+    void  deleteObjectNotFoundException(){
+        when(userRepository.findById(anyInt())).thenThrow(new NotfoundException("Objeto não encontrado"));
+        try {
+            userService.delete(1);
+        }catch (Exception e){
+            assertEquals(NotfoundException.class, e.getClass());
+            assertEquals("Objeto não encontrado", e.getMessage());
+        }
+    }
+
 
     private void startUser(){
         user = new User(1, "Sandoval", "sandovalbento@gmail.com", "1234");
         userDTO = new UserDTO(1, "Sandoval", "sandovalbento@gmail.com", "1234");
-        optionalUser = Optional.of( new User(1, "Sandoval", "sandovalbento@gmail.com", "1234"));
+        optionalUser = Optional.of( new User(1, "Sandoval", "teste@gmail.com", "1234"));
 
     }
 }
